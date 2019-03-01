8ba935f7b67a2a0e538e0321db9351a1af7b6732
liujin <389594458@qq.com>
2019-03-01 23:20:09 +0800

    add a product

---
+++ src/main/java/com/liujin/springbootstartup/domain/Product.java
@@ -0,0 +1,16 @@
+package com.liujin.springbootstartup.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private Long price;
+}
