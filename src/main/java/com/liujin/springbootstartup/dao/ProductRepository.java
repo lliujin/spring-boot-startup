de80bea985db349874b8575a036905fe99225a93
liujin <389594458@qq.com>
2019-03-05 22:01:37 +0800

    supplement ProductService

--- src/main/java/com/liujin/springbootstartup/dao/ProductRepository.java
+++ src/main/java/com/liujin/springbootstartup/dao/ProductRepository.java
@@ -1,8 +1,15 @@
 package com.liujin.springbootstartup.dao;
 
 import com.liujin.springbootstartup.domain.Product;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface ProductRepository extends CrudRepository<Product, Long> {
+import java.util.List;
 
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Transactional
+    void deleteByProductId(Long id);
+
+    List<Product> findFirst5ByPrice();
 }
--- src/main/java/com/liujin/springbootstartup/service/ProductService.java
+++ src/main/java/com/liujin/springbootstartup/service/ProductService.java
@@ -4,7 +4,11 @@ import com.liujin.springbootstartup.dao.ProductRepository;
 import com.liujin.springbootstartup.domain.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
+
+import java.util.Iterator;
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -12,7 +16,7 @@ public class ProductService {
     @Autowired
     ProductRepository productRepository;
 
-    public boolean createProduct(Product product) {
+    boolean createProduct(Product product) {
 
         if(validateProduct(product)) {
             if(productRepository.existsById(product.getId())) {
@@ -37,5 +41,25 @@ public class ProductService {
         } else {
             return true;
         }
+    }
+
+    @Transactional
+    private  boolean deleteByIdList(List<Long> idList) {
+        Iterator i = idList.iterator();
+        while (i.hasNext()) {
+            Product pro = (Product)i.next();
+            Long proId = pro.getId();
+            if(!(productRepository.existsById(proId))) {
+                return false;
+            } else {
+                productRepository.deleteByProductId(proId);
+            }
+        }
+        return true;
+    }
+
+    List<Product> getRecommendedProduct() {
+
+        return productRepository.findFirst5ByPrice();
     }
 }
